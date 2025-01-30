7289b70cfd16fd1a150f7140a10c2483db27183b
Kaifu001 <kaifu.yang@icloud.com>
2025-01-30 17:13:42 +1300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,34 @@
+let circleColor = [255, 0, 0];
+let circleSize = 50;        
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(220); 
 }
 
 function draw() {
-  background(220);
+
+  fill(circleColor);
+  noStroke();
+  ellipse(mouseX, mouseY, circleSize, circleSize);
+}
+
+function mousePressed() {
+  circleColor = [random(255), random(255), random(255)]; 
+}
+
+
+function keyPressed() {
+  if (key === 'R' || key === 'r') {
+    circleSize = 50;
+  } else if (key === 'G' || key === 'g') {
+    circleSize = 80; 
+  } else if (key === 'B' || key === 'b') {
+    circleSize = 30;
+  }
+}
+
+
+function mouseMoved() {
+  circleSize = map(mouseX, 0, width, 20, 100); 
 }
